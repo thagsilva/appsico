c6baeec1200ff78e43e4aede66678c42eedede3d
DiogoFS <diogofs@msn.com>
2024-11-30 20:36:06 -0300

    streamlit roles update

--- appsico_app/streamlit_app.py
+++ appsico_app/streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 if "role" not in st.session_state:
     st.session_state.role = None
 
-ROLES = [None, "Requester", "Responder", "Admin"]
+ROLES = [None, "Assistente", "Psicologista", "Admin"]
 
 
 def login():
@@ -25,50 +25,54 @@ role = st.session_state.role
 
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
-request_1 = st.Page(
-    "request/request_1.py",
-    title="Request 1",
+home = st.Page(
+    "pages/home.py",
+    title="Página inicial",
     icon=":material/help:",
-    default=(role == "Requester"),
+    # default=(role == "Assistente"), 
 )
-request_2 = st.Page(
-    "request/request_2.py", title="Request 2", icon=":material/bug_report:"
+management = st.Page(
+    "pages/management.py",
+    title="Gestão de consultas", 
+    icon=":material/bug_report:",
+    default=(role == "Assistente"),
 )
-respond_1 = st.Page(
-    "respond/respond_1.py",
-    title="Respond 1",
+register = st.Page(
+    "pages/register.py",
+    title="Cadastro de clientes",
     icon=":material/healing:",
-    default=(role == "Responder"),
+    # default=(role == "Assistente"),
 )
-respond_2 = st.Page(
-    "respond/respond_2.py", title="Respond 2", icon=":material/handyman:"
+report = st.Page(
+    "pages/report.py",
+    title="Relatórios",
+    icon=":material/security:",
+    default=(role == "Psicologista"),
 )
 admin_1 = st.Page(
-    "admin/admin_1.py",
+    "pages/admin_1.py",
     title="Admin 1",
     icon=":material/person_add:",
     default=(role == "Admin"),
 )
-admin_2 = st.Page("admin/admin_2.py", title="Admin 2", icon=":material/security:")
 
-account_pages = [logout_page, settings]
-request_pages = [request_1, request_2]
-respond_pages = [respond_1, respond_2]
-admin_pages = [admin_1, admin_2]
+account_pages = [home, logout_page, settings]
+assistent_pages = [management, register]
+psychologist_pages = [report]
+admin_pages = [admin_1]
 
-st.title("Request manager")
-st.logo("images/horizontal_blue.png", icon_image="images/icon_blue.png")
+st.logo("images/logo.png", icon_image="images/logo.png")
 
 page_dict = {}
-if st.session_state.role in ["Requester", "Admin"]:
-    page_dict["Request"] = request_pages
-if st.session_state.role in ["Responder", "Admin"]:
-    page_dict["Respond"] = respond_pages
+if st.session_state.role in ["Assistente", "Admin"]:
+    page_dict["Assistente"] = assistent_pages
+if st.session_state.role in ["Psicologista", "Admin"]:
+    page_dict["Psicologista"] = psychologist_pages
 if st.session_state.role == "Admin":
     page_dict["Admin"] = admin_pages
 
 if len(page_dict) > 0:
-    pg = st.navigation({"Account": account_pages} | page_dict)
+    pg = st.navigation({"Conta": account_pages} | page_dict)
 else:
     pg = st.navigation([st.Page(login)])
 
